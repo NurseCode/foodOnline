f52e34dabb647c036137d926f1fcdc1067c01db4
NurseCode <49458540+NurseCode@users.noreply.github.com>
2023-08-30 15:58:16 -0400

    vendor and customer dashboard edits

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from accounts import views as AccountViews
 
 urlpatterns = [
-    path('registerUser/', views.registerUser, name='registerUser'),
+    # path('registerUser/', views.registerUser, name='registerUser'),
+    path('', AccountViews.vendorDashboard, name='vendor'), 
+    path('profile/', views.vprofile, name='vprofile'),
 ]
