01af18e57fb2f6535da0701cedfbd9d4f4545a33
NurseCode <49458540+NurseCode@users.noreply.github.com>
2023-09-01 07:26:50 -0400

    menu builder category crud

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,4 +6,17 @@ urlpatterns = [
     # path('registerUser/', views.registerUser, name='registerUser'),
     path('', AccountViews.vendorDashboard, name='vendor'), 
     path('profile/', views.vprofile, name='vprofile'),
+    path('menu-builder/', views.menu_builder, name='menu_builder'),
+    path('menu-builder/category/<int:pk>/', views.items_by_category, name='items_by_category'),
+    
+    # Category CRUD
+    path('menu-builder/category/add/', views.add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
+    
+    # Product Item CRUD
+    # path('menu-builder/productItem/add/', views.add_item, name='add_productItem'),
+    # path('menu-builder/productItem/edit/<int:pk>/', views.edit_item, name='edit_productItem'),
+    # path('menu-builder/productItem/delete/<int:pk>/', views.delete_item, name='delete_productItem'),
 ]
+
